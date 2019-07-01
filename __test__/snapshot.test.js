45854c50962b5e3ffd51d5f7eb217877c76907db
William Eduardo Meza <williameduardomeza@gmail.com>
2019-07-01 13:30:34 -0500

    Se agrega una excepción para la snapshot en el id (aletorio numerico)

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -5,4 +5,24 @@ describe('Es hora de las instantaneas', () => {
     test('Snapshot', () => {
         expect(getCharacter(rick)).toMatchSnapshot()
     })
+    /*
+    test('Siempre fallará la instantánea', () => {
+        const user = {
+            createdAt: new Date(),
+            id: Math.floor(Math.random() * 20),
+        }
+        expect(user).toMatchSnapshot()
+    })
+    */
+
+    test('Tenemos una excepción dentro del código', () => {
+        const user = {
+            id: Math.floor(Math.random() * 20),
+            name: "William Meza",
+        }
+        expect(user).toMatchSnapshot({
+            id: expect.any(Number),
+            name: "William Meza"
+        })
+    })
 })
